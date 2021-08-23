f84a10ff8f5060b2555e28c1059466910f43dd3f
Sebi <82907401+Dev-Sebi@users.noreply.github.com>
2021-08-23 14:12:43 +0200

    initial commit

---
+++ handler/index.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const { glob } = require("glob");
+const { promisify } = require("util");
+const { Client } = require("discord.js");
+const globPromise = promisify(glob);
+
+/**
+ * @param {Client} client
+ */
+
+ module.exports = async (client) => {
+
+    const eventFiles = await globPromise(`${process.cwd()}/events/*.js`);
+    eventFiles.map((value) => require(value));
+
+    const ApplicationCommands = await globPromise(`${process.cwd()}/ApplicationCommands/*/*/*.js`);
+
+    const ArrayOfApplicationCommands = [];
+    ApplicationCommands.map((value) => {
+        const file = require(value);
+        if (!file?.name) return;
+        client.ArrayOfApplicationCommands.set(file.name, file);
+        ArrayOfApplicationCommands.push(file);
+    });
+
+
+    client.on("ready", async (client) => {
+        console.log(`Logged in as ${client.user.tag}!`);
+        console.log(`Currently in ${client.guilds.cache.size = 1 ? "Server" : "Servers"}`);
+        // const f = await client.application.commands.fetch()
+        // f.forEach(cmd => cmd.delete())
+        //await client.application.commands.set(ArrayOfApplicationCommands); // if you want to update every guild the server is in (up to 1 hour for the update to complete)
+        await client.guilds.cache.get("850690156582273054").commands.set(ArrayOfApplicationCommands); // if you want to update only one guild (instant update)
+        console.log("Commands Loaded!")
+        client.user.setActivity("Bot Template by Sebi • V1.0.0", { type: "WATCHING" });
+        
+    });
+};
