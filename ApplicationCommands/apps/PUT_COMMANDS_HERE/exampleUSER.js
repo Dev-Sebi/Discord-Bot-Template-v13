0aec421ac81f8e14dd6d061dc58d6e8cfa5fefe1
Sebi <82907401+Dev-Sebi@users.noreply.github.com>
2021-08-24 19:29:36 +0200

    added example commands

---
+++ ApplicationCommands/apps/PUT_COMMANDS_HERE/exampleUSER.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const { Client, CommandInteraction } = require("discord.js");
+const Discord = require("discord.js");
+const fetch = require('node-fetch');
+const emojis = require("../../../utils/emojis");
+const con = require("../../../database/connection");
+const wait = require('util').promisify(setTimeout);
+const moment = require('moment');
+
+module.exports = {
+    name: "ExampleUSER",
+    type: 2, // 2 = User Apps, 3 = Message Apps
+
+    /**
+     *
+     * @param {Client} client
+     * @param {CommandInteraction} interaction
+     * @param {String[]} args
+     */
+    
+    execute: async (client, interaction, args) => {
+        
+        interaction.followUp("User Command")
+
+    },
+};
